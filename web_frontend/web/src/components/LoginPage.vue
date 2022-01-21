6ffe8ba93ca69f4f0f7813f859c9495653acca80
helltf <Dario.Zachow5530@gmail.com>
2022-01-21 10:49:49 +0100

    added Page as main screen

---
+++ web_frontend/web/src/components/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class = "login_component">
+    <LoginField ></LoginField>
+  </div>
+</template>
+
+<script>
+import LoginField from './LoginField.vue' 
+
+export default {
+  name: 'App',
+  components: {
+    LoginField
+  }
+}
+</script>
+<style>
+   @import './LoginPage.css';
+</style>
