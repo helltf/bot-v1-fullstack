1dfa2519d3108f115954192a8bf4b879a43ee336
helltf <Dario.Zachow5530@gmail.com>
2022-01-21 10:50:25 +0100

    added field component

---
+++ web_frontend/web/src/components/InputField.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="default_input">
+        <input id="input_field" v-bind:class="classname" v-bind:type="type"  v-bind:placeholder="placeholder" v-bind:maxlength="maxlength" >
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'InputField',
+  props:{
+      classname:String,
+      type:String,
+      placeholder:String,
+      maxlenght:String
+  }
+}
+
+</script>
+
+<style>
+  @import './InputField.css';
+</style>
