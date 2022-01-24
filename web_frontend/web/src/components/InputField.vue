a86259248ba81f964a408083bdbd18657460b80b
helltf <Dario.Zachow5530@gmail.com>
2022-01-24 16:52:29 +0100

    WIP tried implementing child v-model

--- web_frontend/web/src/components/InputField.vue
+++ web_frontend/web/src/components/InputField.vue
@@ -1,17 +1,27 @@
 <template>
-    <div class="default_input">
-        <input id="input_field" v-bind:class="classname" v-bind:type="type"  v-bind:placeholder="placeholder" v-bind:maxlength="maxlength" >
+<!-- 
+      <div class="default_input">
+        <input class="input_field" :value="v_model" v-bind:id="idname" v-bind:type="type"  v-bind:placeholder="placeholder" v-bind:maxlength="maxlength" >
     </div>
+-->
+<label>
+    <input type="text" :label="label" :value="value" @input="updateValue($event.target.value)" />
+  </label>
 </template>
 
 <script>
-export default {
+export default { 
   name: 'InputField',
-  props:{
-      classname:String,
-      type:String,
-      placeholder:String,
-      maxlenght:String
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  methods:{
+    updateValue(value){
+      this.$emit('input', value)
+    }
   }
 }
 
--- web_frontend/web/src/components/LoginField.vue
+++ web_frontend/web/src/components/LoginField.vue
@@ -2,10 +2,9 @@
   <div class="login_box">
       <h1 class="login_text">LOGIN</h1>
       <div class = input_username_password>
-         <InputField classname="input_username" type="text" placeholder="username..." maxlenght="30" />
-        <InputField classname="input_password" type="password"   placeholder="password..." maxlenght="30" />
+        <InputField :value="password"/>
       </div>
-     <button class="btn_login" type="button">Log in</button>
+     <button v-on:click= "onClick(password)" class="btn_login" type="button" >Log in</button>
   </div>
 </template>
 
@@ -16,6 +15,11 @@ export default {
   name: 'LoginField',
   components:{
     InputField
+  },
+  methods:{
+    onClick (password) {
+      console.log(password)
+    }
   }
 }
 </script>
