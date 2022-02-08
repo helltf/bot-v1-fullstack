bcefe9b4a74b444f3562dd92463ce747bc6748c1
helltf <Dario.Zachow5530@gmail.com>
2022-02-08 16:56:05 +0100

    added new pages

--- web_frontend/web/src/router/index.js
+++ web_frontend/web/src/router/index.js
@@ -1,24 +1,33 @@
-
 import { createRouter, createWebHashHistory } from 'vue-router'
-import LoginPage from "../pages/LoginPage.vue"
-import HomePage from "../pages/HomePage.vue"
+import LoginPage from '../pages/LoginPage.vue'
+import HomePage from '../pages/HomePage.vue'
 
 const routes = [
-    { 
-        path: '/login',
-        name:"Login",
-         component: LoginPage 
-        },
-        { 
-            path: '/',
-            name:"Home",
-             component: HomePage
-            }
-  ]
-  
-  const router = createRouter({
-    history: createWebHashHistory(),
-    routes, 
-  })
+	{
+		path: '/login',
+		name: 'Login',
+		component: LoginPage,
+	},
+	{
+		path: '/',
+		name: 'Home',
+		component: HomePage,
+	},
+	{
+		path: '/about',
+		name: 'About',
+		component: HomePage,
+	},
+	{
+		path: '/weather',
+		name: 'Weather',
+		component: HomePage,
+	},
+]
 
-  export default router
+const router = createRouter({
+	history: createWebHashHistory(),
+	routes,
+})
+
+export default router
