ee2f2028f7541eba685dac7e7a655608aceb7408
helltf <Dario.Zachow5530@gmail.com>
2022-02-08 16:55:58 +0100

    added username

--- web_frontend/web/src/components/LoginField.vue
+++ web_frontend/web/src/components/LoginField.vue
@@ -1,8 +1,9 @@
 <template>
-	<div class="form-wrapper">
+	<div v-if="!loading" class="form-wrapper">
 		<form class="login-form" @keyup.enter="submit(username, password)">
 			<fieldset>
 				<legend>Login</legend>
+				<div class="inner-login">
 				<input
 					class="input-username"
 					type="text"
@@ -20,6 +21,7 @@
 				<button class="submit-btn" @click="submit(username, password)">
 					Submit
 				</button>
+				</div>
 			</fieldset>
 		</form>
 	</div>
@@ -34,21 +36,24 @@ const { cookies } = useCookies()
 export default {
 	name: 'LoginField',
 	async mounted() {
-		let { success } = await checkToken(cookies.get('login'))
+		this.loading = true
+		let { success } = await checkToken(cookies.get('login'), cookies.get('user_login'))
 		if (success) {
 			router.push("/")
 		}
+		this.loading = false
 	},
 	data() {
 		return {
 			username: '',
 			password: '',
 			url: process.env.VUE_APP_URL,
+			loading: false
 		}
 	},
 	methods: {
 		submit: async (username, password) => {
-			let { success, token, username:user_login } = await postLogin(username, password)
+			let { success, token, username:user_login} = await postLogin(username, password)
 			if (success) {
 				cookies.set('login', token, '24h')
 				cookies.set('user_login', user_login, '24h')
@@ -57,8 +62,8 @@ export default {
 		},
 	},
 }
-async function checkToken(token) {
-	return await postData('/token', { token:token })
+async function checkToken(token, user_login) {
+	return await postData('/token', { token,  user_login})
 }
 
 async function postLogin(username, password) {
