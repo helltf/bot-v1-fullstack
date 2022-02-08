ab0a8d54a4bf3f2a429c550efef213a9ce63d860
helltf <Dario.Zachow5530@gmail.com>
2022-02-08 16:55:18 +0100

    created extra component for navbar

---
+++ web_frontend/web/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="nav">
+    <ul>
+      <li><router-link class="link" to="/">Home</router-link></li>
+      <li><router-link class="link" to="/weather">Weather</router-link></li>
+      <li><router-link class="link" to="/about">About</router-link></li>
+      <li><router-link class="link" to="/login">Login</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Navbar"
+}
+</script>
+
+<style>
+    @import './Navbar.css';
+</style>
