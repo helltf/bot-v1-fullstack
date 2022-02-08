dc2550f7c79da13d97486542f536406d7c625660
helltf <Dario.Zachow5530@gmail.com>
2022-02-08 15:10:07 +0100

    added cookie support

--- web_frontend/web/src/main.js
+++ web_frontend/web/src/main.js
@@ -1,9 +1,19 @@
 import {createApp, h} from "vue"
 import App  from "./App.vue"
 import router from './router' 
+import { globalCookiesConfig } from "vue3-cookies";
 
+globalCookiesConfig({
+    expireTimes: "30d",
+    path: "/",
+    domain: "",
+    secure: true,
+    sameSite: "None",
+  });
+  
 const app = createApp({
     render: () => h(App)
 }).use(router)
 
 app.mount('#app')
+
