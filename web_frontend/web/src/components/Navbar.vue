574679ed98dfa06faccef470236aeb9839c999bf
helltf <Dario.Zachow5530@gmail.com>
2022-02-10 13:21:26 +0100

    updated navbar

--- web_frontend/web/src/components/Navbar.vue
+++ web_frontend/web/src/components/Navbar.vue
@@ -1,11 +1,14 @@
 <template>
     <div id="nav">
-    <ul>
-      <li><router-link class="link" to="/">Home</router-link></li>
-      <li><router-link class="link" to="/weather">Weather</router-link></li>
-      <li><router-link class="link" to="/about">About</router-link></li>
-      <li><router-link class="link" to="/login">Login</router-link></li>
+      <div class="nav-wrapper">
+    <ul class="nav-list">
+      <li class="link"><router-link id="link-home"  to="/">Home</router-link></li>
+      <li class="link"><router-link id="link-weather" to="/weather">Weather</router-link></li>
+      <li class="link"><router-link id="link-about" to="/about">About</router-link></li>
+      <li class="link"><router-link id="link-login"  to="/login">Login</router-link></li>
     </ul>
+      </div>
+
   </div>
 </template>
 
