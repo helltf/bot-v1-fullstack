6c6c771adc7b570bde7345f4cd97d94493335306
helltf <Dario.Zachow5530@gmail.com>
2022-02-10 17:47:48 +0100

    reformatting

--- web_frontend/web/src/components/Navbar.vue
+++ web_frontend/web/src/components/Navbar.vue
@@ -1,23 +1,30 @@
 <template>
-    <div id="nav">
-      <div class="nav-wrapper">
-    <ul class="nav-list">
-      <li class="link"><router-link id="link-home"  to="/">Home</router-link></li>
-      <li class="link"><router-link id="link-weather" to="/weather">Weather</router-link></li>
-      <li class="link"><router-link id="link-about" to="/about">About</router-link></li>
-      <li class="link"><router-link id="link-login"  to="/login">Login</router-link></li>
-    </ul>
-      </div>
-
-  </div>
+	<div id="nav">
+		<div class="nav-wrapper">
+			<ul class="nav-list">
+				<li class="link">
+					<router-link id="link-home" to="/">Home</router-link>
+				</li>
+				<li class="link">
+					<router-link id="link-weather" to="/weather">Weather</router-link>
+				</li>
+				<li class="link">
+					<router-link id="link-about" to="/about">About</router-link>
+				</li>
+				<li class="link">
+					<router-link id="link-login" to="/login">Login</router-link>
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-    name:"Navbar"
+	name: 'Navbar',
 }
 </script>
 
 <style>
-    @import './Navbar.css';
+@import './Navbar.css';
 </style>
