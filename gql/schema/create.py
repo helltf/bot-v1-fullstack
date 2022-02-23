dfc71f6e5ee2fb844883ae7b3bd39fabd18bf0bf
helltf <Dario.Zachow5530@gmail.com>
2022-02-23 22:20:30 +0100

    added a create schema function to create the initial schema

---
+++ gql/schema/create.py
@@ -0,0 +1,9 @@
+from graphql.type.schema import GraphQLSchema
+from ariadne import make_executable_schema
+from schema.types import MAIN_TYPEDEF, mutation, query
+from schema.command import COMMAND_TYPEDEF
+
+
+def create_schema() -> GraphQLSchema:
+    print(mutation)
+    return make_executable_schema([MAIN_TYPEDEF, COMMAND_TYPEDEF], [query])
