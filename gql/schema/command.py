4b7184f7404e2ba9845188cd99c41e56f36f4772
helltf <Dario.Zachow5530@gmail.com>
2022-02-23 22:21:08 +0100

    added typedef for command to query commands

---
+++ gql/schema/command.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from schema.types import query
+from graphql import GraphQLResolveInfo
+from data import Command, all_commands, get_command
+
+COMMAND_TYPEDEF = """
+    type Command{
+        name: String!
+        count: Int!
+        required_permissions: Int!
+    }
+"""
+
+
+@query.field("commands")
+def resolve_commands(_, info: GraphQLResolveInfo) -> List[Command]:
+    return all_commands()
+
+
+@query.field("command")
+def resolve_command(_, info: GraphQLResolveInfo, name: str) -> Command:
+    return get_command()
