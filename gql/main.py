be54c0b7d1fe16014b874bbc1770344b1e84052e
helltf <Dario.Zachow5530@gmail.com>
2022-02-24 10:16:01 +0100

    added get endpoint to send error

--- gql/main.py
+++ gql/main.py
@@ -1,8 +1,18 @@
 from ariadne import gql, load_schema_from_path, graphql_sync
 from flask import Flask, jsonify, request
 from schema.create import create_schema
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
+
+
+@app.route('/gql', methods=["GET"])
+def send_error():
+    return {
+               "success": False,
+               "error": "get request are not allowed on this endpoint"
+           }, 400
 
 
 @app.route('/gql', methods=["POST"])
