d100c509fe3deb0c8af99a4ee9d3d03502f86562
helltf <Dario.Zachow5530@gmail.com>
2022-02-24 10:02:33 +0100

    removed unneeded import

--- gql/main.py
+++ gql/main.py
@@ -1,9 +1,9 @@
 from ariadne import gql, load_schema_from_path, graphql_sync
-from flask import Flask,jsonify, request
-from data import all_commands
+from flask import Flask, jsonify, request
 from schema.create import create_schema
+
 app = Flask(__name__)
-all_commands()
+
 
 @app.route('/gql', methods=["POST"])
 def graphql_server():
