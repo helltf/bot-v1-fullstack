a7f028dd6c0aa26be065df88aca4706e52ee8705
helltf <Dario.Zachow5530@gmail.com>
2022-02-24 14:08:42 +0100

    add functions to get commands from database

--- gql/database/database.py
+++ gql/database/database.py
@@ -1,6 +1,29 @@
 from mysql import connector as db
 import os
+from dotenv import load_dotenv
 
-twitch_db = db.connect(
-    host=os.getenv('HOST')
-)
+load_dotenv()
+
+
+def connect_db():
+    return db.connect(
+        host=os.getenv('HOST'),
+        user=os.getenv('USER'),
+        password=os.getenv('PASSWORD'),
+        database=os.getenv('DATABASE')
+    )
+
+
+twitch_db = connect_db()
+
+
+def execute_query_all(query: str):
+    cursor = twitch_db.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+def execute_query_one(query: str):
+    cursor = twitch_db.cursor()
+    cursor.execute(query)
+    return cursor.fetchone()
