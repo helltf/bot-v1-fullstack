547a1411d67aa792e0f86013fc2e503df1c86682
helltf <Dario.Zachow5530@gmail.com>
2022-03-01 21:01:16 +0100

    added new navbar links

--- web_frontend/web/src/components/Navbar.vue
+++ web_frontend/web/src/components/Navbar.vue
@@ -2,33 +2,26 @@
 	<div id="nav">
 		<div class="nav-wrapper">
 			<div class="nav-begin nav-list">
-				<router-link class="link" id="link-home" to="/">Home</router-link>
-				<router-link class="link" id="link-home" to="/">Commands</router-link>
-				<router-link class="link" id="link-weather" to="/weather"
-					>Weather</router-link
-				>
+				<router-link class="link" to="/">Home</router-link>
+				<router-link class="link" to="/commands">Commands</router-link>
+				<router-link class="link" to="/weather">Weather</router-link>
 			</div>
 
 			<div class="nav-end nav-list">
 				<router-link class="link" id="link-about" to="/about"
 					>About</router-link
 				>
-				<div class="dropdown-user" v-if="this.current_user !== null">
-					<router-link class="link" id="link-user" to="/login">{{
-						this.current_user
-					}}</router-link>
-					<div class="dropdown-user-content">
-						<a href="#">Link 1</a>
-						<a href="#">Link 2</a>
-						<a href="#">Link 3</a>
-					</div>
-				</div>
-
 				<router-link
 					class="link"
-					id="link-login"
-					to="/login"
-					v-if="this.current_user === null"
+					to="/user-info"
+					v-if="this.current_user !== null"
+					>{{ this.current_user }}</router-link
+				>
+				<router-link class="link" to="/logout" v-if="this.current_user !== null"
+					>Logout</router-link
+				>
+
+				<router-link class="link" to="/login" v-if="this.current_user === null"
 					>Login</router-link
 				>
 			</div>
@@ -38,21 +31,27 @@
 
 <script>
 import { inject } from '@vue/runtime-core'
-import { useCookies } from 'vue3-cookies'
 import { SIGNED_IN } from '../request/login'
+import router from '../router'
+import { useCookies } from 'vue3-cookies'
 const { cookies } = useCookies()
 
 export default {
 	name: 'Navbar',
-	setup(){
+	setup() {
 		return {
 			current_user: inject('user'),
-			setUser: inject('setUser')
+			setUser: inject('setUser'),
 		}
 	},
-	mounted(){
+	mounted() {
 		this.setUser(cookies.get(SIGNED_IN))
-	}
+	},
+	methods: {
+		async logout() {
+			router.push('/logout')
+		},
+	},
 }
 </script>
 
