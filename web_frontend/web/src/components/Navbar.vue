530e3bd103478505b10d326821598c69541fd394
helltf <Dario.Zachow5530@gmail.com>
2022-03-01 16:51:43 +0100

    added setup function and list on hover

--- web_frontend/web/src/components/Navbar.vue
+++ web_frontend/web/src/components/Navbar.vue
@@ -10,11 +10,25 @@
 			</div>
 
 			<div class="nav-end nav-list">
-				
 				<router-link class="link" id="link-about" to="/about"
 					>About</router-link
 				>
-				<router-link class="link" id="link-login" to="/login" v-if="!this.user.signed_in"
+				<div class="dropdown-user" v-if="this.current_user !== null">
+					<router-link class="link" id="link-user" to="/login">{{
+						this.current_user
+					}}</router-link>
+					<div class="dropdown-user-content">
+						<a href="#">Link 1</a>
+						<a href="#">Link 2</a>
+						<a href="#">Link 3</a>
+					</div>
+				</div>
+
+				<router-link
+					class="link"
+					id="link-login"
+					to="/login"
+					v-if="this.current_user === null"
 					>Login</router-link
 				>
 			</div>
@@ -23,9 +37,22 @@
 </template>
 
 <script>
+import { inject } from '@vue/runtime-core'
+import { useCookies } from 'vue3-cookies'
+import { SIGNED_IN } from '../request/login'
+const { cookies } = useCookies()
+
 export default {
 	name: 'Navbar',
-	inject:["user"]
+	setup(){
+		return {
+			current_user: inject('user'),
+			setUser: inject('setUser')
+		}
+	},
+	mounted(){
+		this.setUser(cookies.get(SIGNED_IN))
+	}
 }
 </script>
 
