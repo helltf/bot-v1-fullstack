82244ec9046c342180a5f1b3717131f848568ac8
helltf <Dario.Zachow5530@gmail.com>
2022-03-01 12:38:48 +0100

    reworked navbar and css

--- web_frontend/web/src/components/Navbar.vue
+++ web_frontend/web/src/components/Navbar.vue
@@ -1,20 +1,23 @@
 <template>
 	<div id="nav">
 		<div class="nav-wrapper">
-			<ul class="nav-list">
-				<li class="link">
-					<router-link id="link-home" to="/">Home</router-link>
-				</li>
-				<li class="link">
-					<router-link id="link-weather" to="/weather">Weather</router-link>
-				</li>
-				<li class="link">
-					<router-link id="link-about" to="/about">About</router-link>
-				</li>
-				<li class="link">
-					<router-link id="link-login" to="/login">Login</router-link>
-				</li>
-			</ul>
+			<div class="nav-begin nav-list">
+				<router-link class="link" id="link-home" to="/">Home</router-link>
+				<router-link class="link" id="link-home" to="/">Commands</router-link>
+				<router-link class="link" id="link-weather" to="/weather"
+					>Weather</router-link
+				>
+			</div>
+
+			<div class="nav-end nav-list">
+				
+				<router-link class="link" id="link-about" to="/about"
+					>About</router-link
+				>
+				<router-link class="link" id="link-login" to="/login"
+					>Login</router-link
+				>
+			</div>
 		</div>
 	</div>
 </template>
