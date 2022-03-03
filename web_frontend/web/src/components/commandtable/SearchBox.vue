cb9461c2e6d458b412c1b52729310abfe8a488c3
helltf <Dario.Zachow5530@gmail.com>
2022-03-03 21:44:32 +0100

    added SearchBox component

---
+++ web_frontend/web/src/components/commandtable/SearchBox.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="command-input">
+		<fieldset class="search-fieldset">
+			<legend>Search</legend>
+			<form class="search-form">
+				<input
+					class="search-input"
+					id="search"
+					type="text"
+					placeholder=""
+                    autocomplete="off"
+					:value="modelValue"
+					@input="$emit('update:modelValue', $event.target.value)"
+				/>
+                <img class="search-img" src="../../assets/search.svg" alt="search">
+			</form>
+		</fieldset>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SearchBox',
+	props: {
+		modelValue: String,
+	},
+}
+</script>
+
+<style>
+@import './SearchBox.css';
+</style>
