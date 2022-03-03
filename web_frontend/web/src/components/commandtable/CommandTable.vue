858f19d33abc4c23251c8fc2223a36de62ada5a9
helltf <Dario.Zachow5530@gmail.com>
2022-03-03 21:44:21 +0100

    updated commandtable: added search input

--- web_frontend/web/src/components/commandtable/CommandTable.vue
+++ web_frontend/web/src/components/commandtable/CommandTable.vue
@@ -1,43 +1,66 @@
 <template>
-	<table class="command-table">
-		<tr>
-			<th @click="orderByKey('name')">Name</th>
-			<th @click="orderByKey('counter')">Counter</th>
-			<th >Description</th>
-			<th @click="orderByKey('permissions')">Required permissions</th>
-			<th >Params (req)</th>
-			<th >Params (opt)</th>
-		</tr>
-		<tr v-for="command of this.commands" :key="command.name">
-			<th>{{ command.name.toLowerCase() }}</th>
-			<th>{{ command.count }}</th>
-			<th>{{ command.description }}</th>
-			<th>{{ command.required_permissions }}</th>
-			<th>{{ command.required_parameters.join(' ') }}</th>
-			<th>{{ command.optional_parameters.join(' ') }}</th>
-		</tr>
-	</table>
+	<div class="table">
+		<SearchBox v-model="search_value"></SearchBox>
+		<table class="command-table" v-if="this.commands.length !== 0">
+			<tr>
+				<th class="clickable" @click="orderByKey('name')">Name</th>
+				<th class="clickable" @click="orderByKey('counter')">Counter</th>
+				<th>Description</th>
+				<th class="clickable" @click="orderByKey('permissions')">
+					Permissions
+				</th>
+				<th>Params (req)</th>
+				<th>Params (opt)</th>
+			</tr>
+			<tr v-for="command of this.commands.values" :key="command.name">
+				<td>{{ command.name.toLowerCase() }}</td>
+				<td class="centered">{{ command.count }}</td>
+				<td>{{ command.description }}</td>
+				<td class="centered">{{ command.required_permissions }}</td>
+				<td>{{ command.required_parameters.join(' ') }}</td>
+				<td>{{ command.optional_parameters.join(' ') }}</td>
+			</tr>
+		</table>
+	</div>
 </template>
 
 <script>
 import { getCommands } from '../../js-functions/gql/commands'
-import {orderBy} from '../../js-functions/order'
+import { orderBy } from '../../js-functions/order'
+import SearchBox from '../commandtable/SearchBox.vue'
+
 export default {
 	name: 'CommandTable',
+	components: {
+		SearchBox,
+	},
 	data() {
 		return {
-			commands: [],
+			commands: {
+				orderedBy: {
+					coloumn: undefined,
+					orderAsc: undefined,
+				},
+				values: [],
+			},
+			search_value: '',
 		}
 	},
 	async mounted() {
-		this.commands = await getCommands()
+		let name = 'name'
+		this.commands.values = await getCommands()
+		this.commands.orderedBy.orderAsc = true
+		this.commands.orderedBy.coloumn = name
+		orderBy(name, this.commands)
 	},
 	methods: {
 		orderByKey(key) {
-      orderBy(key, this.commands)
+			this.commands = orderBy(key, this.commands)
 		},
 	},
 }
 </script>
 
-<style></style>
+<style>
+@import './CommandTable.css';
+</style>
