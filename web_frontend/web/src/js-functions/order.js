1f25681a609335897020eb5cb05e216fe801825a
helltf <Dario.Zachow5530@gmail.com>
2022-03-03 21:45:08 +0100

    refactored order functions

--- web_frontend/web/src/js-functions/order.js
+++ web_frontend/web/src/js-functions/order.js
@@ -1,17 +1,30 @@
 const orderFunctions = {
 	name: (list) => {
-		list.sort((a, b) => a.name.localeCompare(b.name))
+		return list.sort((a, b) => a.name.localeCompare(b.name))
 	},
 	counter: (list) => {
-		list.sort((a, b) => a.count - b.count)
+		return list.sort((a, b) => a.count - b.count)
 	},
 	permissions: (list) => {
-		list.sort((a, b) => a.required_permissions - b.required_permissions)
+		return list.sort((a, b) => a.required_permissions - b.required_permissions)
 	},
 }
 
-const orderBy = (key, list) => {
-	orderFunctions[key](list)
+const orderBy = (coloumn,  {values, orderedBy:{orderAsc}}) => {
+	let ordered_list = orderFunctions[coloumn](values)
+	orderAsc = !orderAsc
+
+	if(!orderAsc){
+		ordered_list.reverse()
+	}
+
+	return {
+		orderedBy:{
+			coloumn,
+			orderAsc
+		},
+		values: ordered_list
+	}
 }
 
 export { orderBy }
