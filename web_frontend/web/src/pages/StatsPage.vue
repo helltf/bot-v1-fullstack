7ec470002f89b1b2787f907f6508ea8787313479
helltf <Dario.Zachow5530@gmail.com>
2022-03-05 12:15:38 +0100

    implemented funtion to run after input

--- web_frontend/web/src/pages/StatsPage.vue
+++ web_frontend/web/src/pages/StatsPage.vue
@@ -1,12 +1,23 @@
 <template>
-    <search-box :modelValue="searchValue" :legendValue="User"/>
+    <search-box :modelValue="searchValue" :legendValue="'User'" @keyup.enter="fetchPlayerStats('helltf')"/>
 </template>
 
 <script>
 import SearchBox from '../components/commandtable/SearchBox.vue'
+
 export default {
     components:{
         SearchBox
+    },
+    data(){
+        return{
+            searchValue:""
+        }
+    },
+    methods:{
+        fetchPlayerStats(player){
+            return getUserStats()
+        }
     }
 }
 </script>
