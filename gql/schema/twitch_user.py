bad6cac369e15dd527094931df1591a8ecc48af8
helltf <Dario.Zachow5530@gmail.com>
2022-03-05 12:40:25 +0100

    reworked types

--- gql/schema/twitch_user.py
+++ gql/schema/twitch_user.py
@@ -2,7 +2,7 @@ from typing import List
 from classes.user_class import User
 from schema.types import query
 from graphql import GraphQLResolveInfo
-from data import Command, all_commands, get_command, get_user
+from data import get_user
 
 TWITCH_USER_TYPEDEF = """
     type User{
@@ -10,12 +10,12 @@ TWITCH_USER_TYPEDEF = """
         id: ID!
         color: String
         permissions: Int!
-        register_time: Int!
+        register_time: String!
         display_name: String
     }
 """
 
 
 @query.field("user")
-def resolve_twitch_user(_, info: GraphQLResolveInfo, **kwargs) -> List[User]:
+def resolve_twitch_user(_, info: GraphQLResolveInfo, **kwargs) -> User:
     return get_user(kwargs)
