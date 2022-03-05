4f28807fcb292996ae3f36c543dc9469ba0bf2ba
helltf <Dario.Zachow5530@gmail.com>
2022-03-05 17:20:36 +0100

    added new query and typedef for stats

--- gql/schema/create.py
+++ gql/schema/create.py
@@ -1,9 +1,8 @@
 from graphql.type.schema import GraphQLSchema
-from ariadne import make_executable_schema
-from schema.types import MAIN_TYPEDEF, mutation, query
+from ariadne import make_executable_schema, load_schema_from_path
+from schema.types import MAIN_TYPEDEF, mutation, query, user
 from schema.command import COMMAND_TYPEDEF
-from schema.twitch_user import TWITCH_USER_TYPEDEF
-
+from schema.twitch_user import TWITCH_USER_TYPEDEF, STATS_TYPEDEF
 
 def create_schema() -> GraphQLSchema:
-    return make_executable_schema([MAIN_TYPEDEF, COMMAND_TYPEDEF, TWITCH_USER_TYPEDEF], [query])
+    return make_executable_schema([MAIN_TYPEDEF, COMMAND_TYPEDEF, TWITCH_USER_TYPEDEF, STATS_TYPEDEF], [query, user])
