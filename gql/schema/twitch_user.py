a9325e5394fef139d6d1854c0f66a2ae125f2168
helltf <Dario.Zachow5530@gmail.com>
2022-03-05 21:08:41 +0100

    moved into stats file

--- gql/schema/twitch_user.py
+++ gql/schema/twitch_user.py
@@ -2,7 +2,7 @@ from typing import List
 from classes.user_class import User
 from schema.types import query, user
 from graphql import GraphQLResolveInfo
-from data import get_user, fetchStats
+from data import get_user, get_stats
 
 TWITCH_USER_TYPEDEF = """
     type User{
@@ -12,23 +12,13 @@ TWITCH_USER_TYPEDEF = """
         permissions: Int!
         register_time: String!
         display_name: String
-        stats: Stats!
+        stats: Stats
     }
 """
 
-STATS_TYPEDEF = """
-    type Stats{
-        name: String!
-        id:ID!
-    }
-"""
 
 
 @query.field("user")
 def resolve_twitch_user(_, info: GraphQLResolveInfo, **kwargs) -> User:
     return get_user(kwargs)
 
-
-@user.field("stats")
-def resolve(parent, info: GraphQLResolveInfo, **kwargs):
-    return fetchStats()
