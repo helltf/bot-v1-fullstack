af52e161f749ebf787aac4ba9784ac13af1cdd0a
helltf <Dario.Zachow5530@gmail.com>
2022-03-05 22:14:25 +0100

    added function to get rps stats

--- gql/database/stats_db.py
+++ gql/database/stats_db.py
@@ -1,7 +1,12 @@
-from classes.stats_classes import ColorHistory
+from classes.stats_classes import ColorHistory, Rps
 from database.database import get_one
 
 
 def get_color_history(id):
     raw = get_one("COLOR_HISTORY", {"twitch_id": id})
     return ColorHistory(raw)
+
+
+def get_rps_stats(id):
+    raw = get_one("RPS_STATS", {"twitch_id": id})
+    return Rps(raw)
