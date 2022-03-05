e0c3976e3495cbaa3d8b1ea133759007c155b084
helltf <Dario.Zachow5530@gmail.com>
2022-03-05 12:14:52 +0100

    added def to get userdata from db

--- gql/data.py
+++ gql/data.py
@@ -1,6 +1,7 @@
 from typing import Dict, Any
 
 from classes.command_class import Command
+from classes.user_class import User
 from database import database
 
 
@@ -10,9 +11,22 @@ def all_commands():
     return commands
 
 
-def get_command(filter_info: Dict[str, Any]):
-    query = "SELECT * FROM commands"
-    for k, v in filter_info.items():
-        query += " WHERE {} = %s".format(k)
-    raw_command = database.execute_query_one(query, list(filter_info.values()) )
+def get_command(filter: Dict[str, Any]):
+    raw_command = get_one("commands", filter)
     return Command(raw_command)
+
+
+def get_user(filter: Dict[str, Any]):
+    raw_user = get_one("TWITCH_USER", filter)
+    return User(raw_user)
+
+
+def get_one(tablename: str, filter: Dict[str, Any]):
+    query = "SELECT * FROM {}".format(tablename)
+
+    for k, v in filter.items():
+        query += " WHERE {} = %s".format(k)
+
+    raw = database.execute_query_one(query, list(filter.values()))
+
+    return raw
