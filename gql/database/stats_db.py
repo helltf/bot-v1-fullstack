aa3a73e18b260823f9b2e8bf16e68911045cba1a
helltf <Dario.Zachow5530@gmail.com>
2022-03-05 22:08:45 +0100

    working function

--- gql/database/stats_db.py
+++ gql/database/stats_db.py
@@ -1,2 +1,7 @@
+from classes.stats_classes import ColorHistory
+from database.database import get_one
+
+
 def get_color_history(id):
-    return {}
+    raw = get_one("COLOR_HISTORY", {"twitch_id": id})
+    return ColorHistory(raw)
