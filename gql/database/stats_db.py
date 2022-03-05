eb9a8a3f0230b538c2dbe696b15dd35711dce772
helltf <Dario.Zachow5530@gmail.com>
2022-03-05 23:30:55 +0100

    added function to get cookie stats

--- gql/database/stats_db.py
+++ gql/database/stats_db.py
@@ -1,5 +1,5 @@
-from classes.stats_classes import ColorHistory, Rps
-from database.database import get_one
+from classes.stats_classes import ColorHistory, Rps, Cookie
+from database.database import get_one, execute_query_all
 
 
 def get_color_history(id):
@@ -10,3 +10,16 @@ def get_color_history(id):
 def get_rps_stats(id):
     raw = get_one("RPS_STATS", {"twitch_id": id})
     return Rps(raw)
+
+
+def get_cookie_stats(name):
+    amount = 0
+    raw_cookies = execute_query_all("SELECT * FROM COOKIES WHERE USERNAME = %s", [name])
+    for entry in raw_cookies:
+        amount += entry[1]
+
+    average = round(amount / len(raw_cookies))
+
+    reset_count = execute_query_all("SELECT COUNT(*) FROM COOKIE_RESET WHERE USERNAME = %s", [name])
+
+    return Cookie(amount, reset_count[0][0], average)
