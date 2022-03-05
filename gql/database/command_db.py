a42e271ea78893ea6c3752b388d81291842969aa
helltf <Dario.Zachow5530@gmail.com>
2022-03-05 23:32:11 +0100

    fixed error unkown imports

--- gql/database/command_db.py
+++ gql/database/command_db.py
@@ -1,12 +1,11 @@
 from typing import Dict, Any
 
 from classes.command_class import Command
-from database.database import get_one
-from database import database
+from database.database import get_one, execute_query_one, execute_query_all
 
 
 def all_commands():
-    raw_command_list = database.execute_query_all("SELECT * FROM commands")
+    raw_command_list = execute_query_all("SELECT * FROM commands")
     commands = map(lambda x: Command(x), raw_command_list)
     return commands
 
