c6adb404646feff34f97a3126aeb0abe81699142
helltf <Dario.Zachow5530@gmail.com>
2022-03-05 12:14:07 +0100

    added user typedef

--- gql/schema/create.py
+++ gql/schema/create.py
@@ -2,7 +2,8 @@ from graphql.type.schema import GraphQLSchema
 from ariadne import make_executable_schema
 from schema.types import MAIN_TYPEDEF, mutation, query
 from schema.command import COMMAND_TYPEDEF
+from schema.twitch_user import TWITCH_USER_TYPEDEF
 
 
 def create_schema() -> GraphQLSchema:
-    return make_executable_schema([MAIN_TYPEDEF, COMMAND_TYPEDEF], [query])
+    return make_executable_schema([MAIN_TYPEDEF, COMMAND_TYPEDEF, TWITCH_USER_TYPEDEF], [query])
