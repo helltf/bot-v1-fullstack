c572554950720000e494fcd3942fc2cd7f0e26ca
helltf <Dario.Zachow5530@gmail.com>
2022-03-05 17:21:08 +0100

    added user as objecttype

--- gql/schema/types.py
+++ gql/schema/types.py
@@ -3,11 +3,11 @@ from ariadne import MutationType, QueryType, ObjectType
 MAIN_TYPEDEF = """
     type Query {
         commands:[Command]
-        command(name:String, id:ID):Command
-        user(username:String, twitch_id:ID): User
+        command(name:String, id:ID):Command!
+        user(username:String, twitch_id:ID): User!
     }
-    
     """
 
 query = QueryType()
+user = ObjectType("User")
 mutation = MutationType()
