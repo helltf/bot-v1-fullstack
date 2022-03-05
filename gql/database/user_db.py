26a773704e5d22bb111d7bfd4a255647eb06dcb6
helltf <Dario.Zachow5530@gmail.com>
2022-03-05 22:52:19 +0100

    added mapper function to accept name or id

--- gql/database/user_db.py
+++ gql/database/user_db.py
@@ -10,3 +10,21 @@ def get_user(filter: Dict[str, Any]):
 
     raw_user = get_one("TWITCH_USER", filter)
     return User(raw_user)
+
+
+def map_id_to_name(id):
+    result = get_one("TWITCH_USER", {"twitch_id": id})
+    
+    if result is None:
+        return
+
+    return {"id": id, "name": result[0]}
+
+
+def map_name_to_id(name):
+    result = get_one("TWITCH_USER", {"username": name})
+
+    if result is None:
+        return None
+
+    return {"id": result[1], "name": name}
