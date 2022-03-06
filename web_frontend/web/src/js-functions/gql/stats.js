649b69fb72d418123d942a17df435d263e8b1d66
helltf <Dario.Zachow5530@gmail.com>
2022-03-06 18:09:59 +0100

    added query to fetch user stats

--- web_frontend/web/src/js-functions/gql/stats.js
+++ web_frontend/web/src/js-functions/gql/stats.js
@@ -1,9 +1,40 @@
 import { request, gql } from 'graphql-request'
 
 
-const getUserStats = (user) => {
+const getUserStats = async (user) => {
     const query = gql`
+    query{
+        user(username:"${user}"){
+            id,
+            username,
+            color,
+            permissions,
+            register_time,
+            display_name,
+            stats{
+                color_history{
+                    last_change,
+                    history,
+                    register_time
+                },
+                rps{
+                    draw, win, lose
+                },
+                cookie{
+                    amount, average, resets
+                },
+                ban{
+                    amount
+                },
+                timeout{
+                    amount
+                }
     
+            }
+        }
+    }
     `
-    return {}
+    return await request(process.env.VUE_APP_GQL_URL, query)
 }
+
+export {getUserStats}
