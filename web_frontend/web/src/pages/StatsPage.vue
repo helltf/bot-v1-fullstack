0edd791200acebb9a35e6b95f9b2b63eaee41b28
helltf <Dario.Zachow5530@gmail.com>
2022-03-06 19:49:20 +0100

    updated function to get data

--- web_frontend/web/src/pages/StatsPage.vue
+++ web_frontend/web/src/pages/StatsPage.vue
@@ -1,28 +1,39 @@
 <template>
-    <search-box v-model="searchValue" :legendValue="'User'" @keyup.enter="fetchPlayerStats()"/>
+	<search-box
+		v-model="searchValue"
+		:legendValue="'User'"
+		@keyup.enter="fetchPlayerStats()"
+	/>
+	<h1 v-if="this.errorMessage">{{ errorMessage }}</h1>
 </template>
 
 <script>
 import SearchBox from '../components/commandtable/SearchBox.vue'
-import {getUserStats} from '../js-functions/gql/stats'
+import { getUserStats } from '../js-functions/gql/stats'
 
 export default {
-    components:{
-        SearchBox
-    },
-    data(){
-        return{
-            searchValue:""
-        }
-    },
-    methods:{
-        fetchPlayerStats(){
-            return getUserStats(this.searchValue)
-        }
-    }
+	components: {
+		SearchBox,
+	},
+	data() {
+		return {
+			searchValue: '',
+			errorMessage: '',
+		}
+	},
+	methods: {
+		async fetchPlayerStats() {
+			let { success, error, data } = await getUserStats(this.searchValue)
+			console.log(success, error, data)
+			if (success) {
+				this.errorMessage = ''
+                
+			} else {
+				this.errorMessage = "Cannot fetch user for given input"
+			}
+		},
+	},
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
