285d14c2a63fda6b0ad3ff2149e430ea331a3488
helltf <Dario.Zachow5530@gmail.com>
2022-03-07 23:09:50 +0100

    added new card component

---
+++ web_frontend/web/src/components/infocard/InfoCard.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="item-card">
+		<h1>{{ title }}</h1>
+		<info-list :items="data" />
+	</div>
+</template>
+
+<script>
+import InfoList from '../infolist/InfoList.vue'
+
+export default {
+	name: 'InfoCard',
+	props: {
+		data: Object,
+		title: String,
+	},
+	components: {
+		InfoList,
+	},
+	mounted() {
+		console.log(this.data)
+	},
+}
+</script>
+
+<style></style>
