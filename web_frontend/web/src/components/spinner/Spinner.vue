6a629ac55a034faedc643943f3bc989b340f84a8
helltf <Dario.Zachow5530@gmail.com>
2022-03-09 18:45:34 +0100

    added Spinner component

---
+++ web_frontend/web/src/components/spinner/Spinner.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="spinner">
+		<div class="lds-facebook">
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Spinner',
+}
+</script>
+
+<style>
+@import './Spinner.css';
+</style>
