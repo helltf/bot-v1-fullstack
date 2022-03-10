d25cf2cad2ef7d522c219c3f4114195bff765ce6
helltf <Dario.Zachow5530@gmail.com>
2022-03-10 22:17:12 +0100

    added new component to replace Stats list

---
+++ web_frontend/web/src/components/statslist/StatsList.vue
@@ -0,0 +1,26 @@
+<template>
+	<info-card :title="'User info'" :data="this.userInfo" />
+	<info-card
+		:data="value"
+		:title="name"
+		v-for="(value, name) in this.stats"
+		:key="name"
+	/>
+</template>
+
+<script>
+import InfoCard from '../infocard/InfoCard.vue'
+
+export default {
+	name: 'StatsList',
+	components: {
+		InfoCard,
+	},
+    props:{
+        stats: Object,
+        userInfo: Object
+    }
+}
+</script>
+
+<style></style>
