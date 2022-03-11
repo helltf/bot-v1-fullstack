fce8b24e7ad787002e2a147f49737bc0f3165f01
helltf <Dario.Zachow5530@gmail.com>
2022-03-11 09:50:21 +0100

    added new login buttons

--- web_frontend/web/src/components/loginfield/LoginField.vue
+++ web_frontend/web/src/components/loginfield/LoginField.vue
@@ -1,42 +1,45 @@
 <template>
-<div class="wrapper">
-	<div class="top-container"></div>
-<div class="container">
-	<div v-if="!loading" class="form-wrapper">
-		<form class="login-form" @keyup.enter="submit(username, password)">
-			<fieldset class="login-fieldset">
-				<legend>Login</legend>
-				<span class="error-msg">{{errormessage}}</span>
-				<div class="inner-login">
-				<input
-					class="input-username"
-					type="text"
-					required
-					v-model="username"
-					placeholder="Enter email"
-				/>
-				<input
-					class="input-password"
-					type="password"
-					required
-					v-model="password"
-					placeholder="Password"
-				/>
-				<button class="submit-btn" @click="submit(username, password)">
-					Log in
-				</button>
-				</div>
-			</fieldset>
-		</form>
+	<div class="wrapper">
+		<div class="top-container"></div>
+		<div class="container">
+			<div v-if="!loading" class="form-wrapper">
+				<form class="login-form" @keyup.enter="submit(username, password)">
+					<fieldset class="login-fieldset">
+						<legend>Login</legend>
+						<div class="inner-login">
+							<input
+								class="input-username"
+								type="text"
+								required
+								v-model="username"
+								placeholder="Enter email"
+							/>
+							<input
+								class="input-password"
+								type="password"
+								required
+								v-model="password"
+								placeholder="Password"
+							/>
+							<div class="login-btns">
+								<button class="submit-btn" @click="submit(username, password)">
+									Log in
+								</button>
+							</div>
+							<a class="submit-btn" v-bind:href="getTwitchLoginUrl">
+								Twich Login
+							</a>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+		</div>
 	</div>
-</div>
-</div>
 </template>
 
 <script>
-
 import router from '../../router'
-import {postLogin, TOKEN, SIGNED_IN} from '../../js-functions/request/login'
+import { postLogin, TOKEN, SIGNED_IN } from '../../js-functions/request/login'
 import { inject } from '@vue/runtime-core'
 import { useCookies } from 'vue3-cookies'
 const { cookies } = useCookies()
