0d39c87809d226fe03146a05f48af80be4faf4b0
helltf <Dario.Zachow5530@gmail.com>
2022-03-13 23:27:01 +0100

    moved logic from component into page

--- web_frontend/web/src/pages/LoginPage.vue
+++ web_frontend/web/src/pages/LoginPage.vue
@@ -1,16 +1,28 @@
 <template>
-    <LoginField ></LoginField>
+	<LoginField></LoginField>
 </template>
 
 <script>
-import LoginField from '../components/loginfield/LoginField.vue' 
+import LoginField from '../components/loginfield/LoginField.vue'
+import router from '../router'
+import { inject } from '@vue/runtime-core'
 
 export default {
-  name: 'LoginPage',
-  components: {
-    LoginField
-  }
+	name: 'LoginPage',
+	components: {
+		LoginField,
+	},
+	setup() {
+		return {
+			current_user: inject('user'),
+			setUser: inject('setUser'),
+		}
+	},
+	mounted() {
+		if (this.current_user !== undefined) {
+			router.push('/')
+		}
+	},
 }
 </script>
-<style>
-</style>
+<style></style>
