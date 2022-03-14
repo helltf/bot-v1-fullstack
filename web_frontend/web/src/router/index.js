c1083b32f6da92084589cb4791b39164a8a1cb17
helltf <Dario.Zachow5530@gmail.com>
2022-03-14 15:04:23 +0100

    back to WebHash

--- web_frontend/web/src/router/index.js
+++ web_frontend/web/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginPage from '../pages/LoginPage.vue'
 import HomePage from '../pages/HomePage.vue'
 import LogoutPage from '../pages/LogoutPage.vue'
@@ -55,7 +55,7 @@ const routes = [
 ]
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes,
 })
 
