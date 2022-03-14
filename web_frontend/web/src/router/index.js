8a3a20b6b378d4f359b3cdfa66d7fc093bf1fe6c
helltf <Dario.Zachow5530@gmail.com>
2022-03-14 13:08:12 +0100

    fixed refreshing on submit

--- web_frontend/web/src/router/index.js
+++ web_frontend/web/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../pages/LoginPage.vue'
 import HomePage from '../pages/HomePage.vue'
 import LogoutPage from '../pages/LogoutPage.vue'
@@ -55,7 +55,7 @@ const routes = [
 ]
 
 const router = createRouter({
-	history: createWebHashHistory(),
+	history: createWebHistory(),
 	routes,
 })
 
