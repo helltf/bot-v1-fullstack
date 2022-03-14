15b4fdb37aea7bf8b61782656af3474685e19a6a
helltf <Dario.Zachow5530@gmail.com>
2022-03-14 16:35:21 +0100

    added new function to query exactly one field

--- web_frontend/web/src/js-functions/gql/stats.js
+++ web_frontend/web/src/js-functions/gql/stats.js
@@ -1,37 +1,41 @@
 import { gql } from 'graphql-request'
-import {request} from './graphql-request'
+import { request } from './graphql-request'
 
-const userFields = `id, username, color, permissions, register_time, display_name,`
-const colorHistoryFields = `last_change, history, register_time`
-const rpsFields = `draw, win, lose`
-const cookieFields = `amount, average, resets`
+const availableFields = {
+	userFields: `id, username, color, permissions, register_time, display_name`,
+	color_history: `last_change, history, register_time`,
+	rps: `draw, win, lose`,
+	cookie: `amount, average, resets`,
+	ban: `amount`,
+	timeout: `amount`,
+}
 
-const getQueryParams = (user) =>{
-    return isNaN(user) ? `username:"${user}"` : `twitch_id:"${Number(user)}"`
+const getUserQueryParam = (user) => {
+	user = user.trim()
+	return isNaN(user) ? `username:"${user}"` : `twitch_id:"${Number(user)}"`
 }
 
 const getStatsQuery = (user) => {
-
-    const queryParams = getQueryParams(user.trim())
+	const queryParams = getUserQueryParam(user)
 	return gql`
     query{
         user(${queryParams}){
-            ${userFields}
+            ${availableFields.userFields}
             stats{
                 color_history{
-                    ${colorHistoryFields}
+                    ${availableFields.color_history}
                 },
                 rps{
-                    ${rpsFields}
+                    ${availableFields.rps}
                 },
                 cookie{
-                    ${cookieFields}
+                    ${availableFields.cookie}
                 },
                 ban{
-                    amount
+                    ${availableFields.ban}
                 },
                 timeout{
-                    amount
+                    ${availableFields.timeout}
                 }
     
             }
@@ -40,10 +44,40 @@ const getStatsQuery = (user) => {
     `
 }
 
+const fetchUserInfo = (user) => {
+	const queryParams = getUserQueryParam(user)
+
+	const query = gql`
+        query{
+            user(${queryParams}){
+                ${availableFields.userFields}
+            }
+        }
+    `
+	return request(process.env.VUE_APP_GQL_URL, query)
+}
+
+const fetchStatsField = (user, field) => {
+	const queryParams = getUserQueryParam(user)
+
+	const query = gql`
+        query{
+            user(${queryParams}){
+                stats{
+                    ${field}{
+                        ${availableFields[field]}
+                    }
+                }
+            }
+        }
+    `
+	return request(process.env.VUE_APP_GQL_URL, query)
+}
+
 const getUserStats = async (user) => {
 	const query = getStatsQuery(user)
 
-    return request(process.env.VUE_APP_GQL_URL, query)
+	return request(process.env.VUE_APP_GQL_URL, query)
 }
 
-export { getUserStats }
+export { getUserStats, getUserQueryParam, fetchUserInfo, fetchStatsField }
