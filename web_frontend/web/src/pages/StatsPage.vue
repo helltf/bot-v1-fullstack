d2bc2eb816f57ec07596107db603de72d306fc99
helltf <Dario.Zachow5530@gmail.com>
2022-03-14 16:35:04 +0100

    moved fetching of data into children

--- web_frontend/web/src/pages/StatsPage.vue
+++ web_frontend/web/src/pages/StatsPage.vue
@@ -4,31 +4,23 @@
 		:legendValue="'User'"
 		@keyup.enter="setUserInput()"
 	/>
-	<div v-if="this.loading" class="loading-container">
-		<spinner />
-	</div>
-	<div v-else-if="!this.loading && this.setUser" class="loaded-container">
-		<stats-list :stats="getStats" :userInfo="getUserInfo" />
+	<div v-if="!this.loading && this.setUser" class="loaded-container">
+		<stats-list :userInput="this.setUser"/>
 	</div>
 </template>
 
 <script>
 import SearchBox from '../components/commandtable/SearchBox.vue'
-import { getUserStats } from '../js-functions/gql/stats'
 import StatsList from '../components/statslist/StatsList.vue'
-import { errorNotification } from '../js-functions/notification'
-import Spinner from '../components/spinner/Spinner.vue'
 
 export default {
 	components: {
 		SearchBox,
-		Spinner,
 		StatsList,
 	},
 	data() {
 		return {
 			searchValue: '',
-			data: null,
 			loading: false,
 			setUser: '',
 		}
@@ -36,40 +28,8 @@ export default {
 	methods: {
 		setUserInput() {
 			this.setUser = this.searchValue
-			this.fetchPlayerStats()
 		},
-		
-		async fetchPlayerStats() {
-			this.loading = true
-			let { success, data } = await getUserStats(this.searchValue)
-			if (success) {
-				this.data = data.user
-			} else {
-				errorNotification({
-					title: 'Stats',
-					text: `No data found for user "${this.searchValue}"`,
-				})
-			}
-			this.loading = false
-		},
-	},
-	computed: {
-		getStats() {
-			if (!this.data) return {}
-			return this.data.stats
-		},
-		getUserInfo() {
-			if (this.data === null) return {}
-
-			let object = {
-				...this.data,
-			}
-
-			delete object.stats
-
-			return object
-		},
-	},
+	}
 }
 </script>
 
