7a9692f3ee77a4155d32ee8e601e69db764a336e
helltf <Dario.Zachow5530@gmail.com>
2022-03-14 16:35:44 +0100

    updated for new purposes

--- web_frontend/web/src/components/statslist/StatsList.vue
+++ web_frontend/web/src/components/statslist/StatsList.vue
@@ -1,25 +1,59 @@
 <template>
-	<info-card :title="'User info'" :data="this.userInfo" />
-	<info-card
-		:data="value"
-		:title="name"
-		v-for="(value, name) in this.stats"
-		:key="name"
-	/>
+	<div v-if="this.loading" class="loading-container">
+		<spinner />
+	</div>
+	<div v-else>
+		<stats-card
+			:field="'user_info'"
+			:title="'User info'"
+			:user="this.userInput"
+		/>
+		<stats-card
+			:title="title"
+			:field="field"
+			:user="this.userInput"
+			v-for="[title, field] of this.statsFields"
+			:key="field"
+		/>
+	</div>
 </template>
 
 <script>
-import InfoCard from '../infocard/InfoCard.vue'
+import StatsCard from '../statslist/StatsCard.vue'
+import Spinner from '../spinner/Spinner.vue'
+import { fetchUserInfo } from '../../js-functions/gql/stats'
+
+const statsFields = new Map([
+	['Recent Colors', 'color_history'],
+	['Cookie stats', 'cookie'],
+	['Rps games', 'rps'],
+	['Recieved timeouts', 'timeout'],
+	['Recieved bans', 'ban'],
+])
 
 export default {
 	name: 'StatsList',
 	components: {
-		InfoCard,
+		Spinner,
+		StatsCard,
 	},
-    props:{
-        stats: Object,
-        userInfo: Object
-    }
+	props: {
+		userInput: String,
+	},
+	data() {
+		return {
+			userInfo: {},
+			loading: false,
+			statsFields: statsFields,
+		}
+	},
+	async mounted() {
+		this.loading = true
+		let { data } = await fetchUserInfo(this.userInput)
+		this.userInfo = data
+
+		this.loading = false
+	},
 }
 </script>
 
