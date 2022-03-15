3cede6f13c5ec017a81b68719ff53975e489fc3e
helltf <Dario.Zachow5530@gmail.com>
2022-03-15 15:37:04 +0100

    now parsing user as route param

--- web_frontend/web/src/pages/StatsPage.vue
+++ web_frontend/web/src/pages/StatsPage.vue
@@ -2,16 +2,17 @@
 	<search-box
 		v-model="searchValue"
 		:legendValue="'User'"
-		@keyup.enter="setUserInput()"
+		@keyup.enter="openStatsPage()"
 	/>
-	<div v-if="!this.loading && this.setUser" class="loaded-container">
-		<stats-list :userInput="this.setUser"/>
+	<div v-if="this.setUser !== ''">
+		<stats-list :userInput="this.setUser" />
 	</div>
 </template>
 
 <script>
 import SearchBox from '../components/commandtable/SearchBox.vue'
 import StatsList from '../components/statslist/StatsList.vue'
+import router from '../router'
 
 export default {
 	components: {
@@ -21,15 +22,26 @@ export default {
 	data() {
 		return {
 			searchValue: '',
-			loading: false,
 			setUser: '',
 		}
 	},
 	methods: {
-		setUserInput() {
-			this.setUser = this.searchValue
+		openStatsPage() {
+			router.push('/stats/' + this.searchValue)
 		},
+		getUserRouteParam(){
+			return this.$route.params.user ? this.$route.params.user : ''
+		}
+	},
+	updated() {
+		if (this.getUserRouteParam() !== undefined) {
+			this.setUser = this.getUserRouteParam()
+		}
+	},
+	mounted(){
+		this.setUser = this.getUserRouteParam()
 	}
+
 }
 </script>
 
