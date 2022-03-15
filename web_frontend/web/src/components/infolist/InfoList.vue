c29e92546bb5545f5b0ad9140ab38ffa236faebd
helltf <Dario.Zachow5530@gmail.com>
2022-03-15 15:39:38 +0100

    added span to be shown if no data is given

--- web_frontend/web/src/components/infolist/InfoList.vue
+++ web_frontend/web/src/components/infolist/InfoList.vue
@@ -1,17 +1,22 @@
 <template>
-  <ul class="info-list" v-for="(value, name) in this.items" :key="name">
-      <li>{{value}} {{name}}</li>
-  </ul>
+	<div class="list-content">
+		<ul class="info-list" v-for="(value, name) in this.items" :key="name">
+			<li>{{ value }} {{ name }}</li>
+		</ul>
+	</div>
+	<div class="list-no-content" v-if="!this.items">
+		<span class="list-empty" >No Record found</span>
+	</div>
 </template>
 
 <script>
 export default {
-    props:{
-        items: Object
-    }
+	props: {
+		items: Object,
+	}
 }
 </script>
 
 <style>
-
+@import './InfoList.css';
 </style>
