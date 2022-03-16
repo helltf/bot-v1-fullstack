357a09fc532288740c8325629642550fd0a30ad3
helltf <Dario.Zachow5530@gmail.com>
2022-03-16 23:02:21 +0100

    added component to display colors

---
+++ web_frontend/web/src/components/colordisplay/ColorDisplay.vue
@@ -0,0 +1,35 @@
+<template>
+	<div
+		:key="color"
+		v-for="color of getColors"
+		:style="this.colorStyle(color)"
+		class="color-display"
+	></div>
+</template>
+
+<script>
+export default {
+	name: 'ColorDisplay',
+	props: ['color'],
+	computed: {
+		getColors() {
+			if (typeof this.color === 'string') {
+				return [this.color]
+			} else {
+				return [...this.color]
+			}
+		},
+	},
+	methods: {
+		colorStyle(color) {
+			return {
+				'background-color': color,
+			}
+		},
+	},
+}
+</script>
+
+<style>
+@import './ColorDisplay.css';
+</style>
