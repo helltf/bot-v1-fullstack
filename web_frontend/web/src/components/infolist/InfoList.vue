85fbe019b8096fbc159ea5dbfa2c8bdc3c1a0adf
helltf <Dario.Zachow5530@gmail.com>
2022-03-16 16:47:32 +0100

    fieldset to show data

--- web_frontend/web/src/components/infolist/InfoList.vue
+++ web_frontend/web/src/components/infolist/InfoList.vue
@@ -1,11 +1,12 @@
 <template>
 	<div class="list-content" v-if="this.items">
-		<ul class="info-list" v-for="(value, name) in this.items" :key="name">
-			<li>
-				<span class="info-name" >{{ name }}</span>
-				<span class="info-value" >{{ value }}</span>
-			</li>
-		</ul>
+		<div class="info-list" v-for="(value, name) in this.items" :key="name">
+			<fieldset class="info-fieldset">
+				<legend class="info-legend">{{ name }}</legend>
+				<span class="info-value">{{ value }}</span>
+			</fieldset>
+			<!-- <span class="info-name">{{ name }}</span> -->
+		</div>
 	</div>
 	<div v-else class="list-empty-container">
 		<span class="list-empty">No Record found</span>
