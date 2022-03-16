01e33503d347eb0d0fa38e833c92f9bbb0c8ca60
helltf <Dario.Zachow5530@gmail.com>
2022-03-16 23:01:42 +0100

    added constants

---
+++ web_frontend/web/src/js-functions/constants/stats-constants.js
@@ -0,0 +1,10 @@
+const title = {
+    'display_name': 'display name',
+    'register_time': 'registered since',
+    'last_change': 'last change',
+    'first_channel':'first in channel',
+    'last_channel': 'last in channel'
+}
+
+
+export {title as titleReplacements}
