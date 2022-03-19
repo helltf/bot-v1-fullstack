ce64cff012418558263f3a924416d9098f92766a
helltf <Dario.Zachow5530@gmail.com>
2022-03-19 17:24:33 +0100

    updated script

--- web_frontend/web/deploy.sh
+++ web_frontend/web/deploy.sh
@@ -4,10 +4,16 @@ set -e
 
 npm run build
 
-cd dist
 
-git init
-git add -A
+cd ~/Desktop/deploy_vue_app/bot 
+rm -rf !(".git")
+
+cd /var/www/inst_mono/web_frontend/web
+#cp -R dist/* /home/pi/Desktop/deploy_vue_app/
+
+cd ~/Desktop/deploy_vue_app/bot 
+
+git add .
 git commit -m 'deploy'
 
 git push -f git@github.com:helltf/bot.git main:gh-pages
