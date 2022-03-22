5ade9392228cb8066f9d56000e48d8c5ea77b0f6
helltf <Dario.Zachow5530@gmail.com>
2022-03-22 13:32:37 +0100

    wrapped content into div

--- web_frontend/web/src/pages/HomePage.vue
+++ web_frontend/web/src/pages/HomePage.vue
@@ -1,9 +1,11 @@
 <template>
   <div class = "home">
+    <div class="home-content">
     <h1>Welcome to my webpage</h1>
     <h1>About myself</h1>
     <h1>My Projects</h1>
     <h1>My Social Media</h1>
+    </div>
   </div>
 </template>
 
@@ -16,7 +18,19 @@ export default {
 }
 </script>
 <style>
-
+  .home-content{
+    background: var(--default-background);
+    height: 100%;
+    width: 70%;
+  }
+  .home{
+    display: flex;
+    justify-content: center;
+    height: 100%;
+  }
+  #app{
+    height: 100%;
+  }
 </style>
 
 
