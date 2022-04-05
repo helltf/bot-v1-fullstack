3131fe79cb5782723a3e105006a405855ef6f12c
helltf <Dario.Zachow5530@gmail.com>
2022-04-05 20:42:26 +0200

    created homecontainer

---
+++ web_frontend/web/src/components/homecontainer/HomeContainer.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="home">
+		<div class="home-content">
+			<welcome-container/>
+			<h1>About myself</h1>
+			<h1>My Projects</h1>
+			<h1>My Social Media</h1>
+		</div>
+	</div>
+</template>
+
+<script>
+
+import WelcomeContainer from './WelcomeContainer.vue'
+
+export default {
+    name: 'HomeContainer',
+    components:{
+        WelcomeContainer
+    }
+}
+</script>
+
+<style>
+ @import './HomeContainer.css'
+</style>
